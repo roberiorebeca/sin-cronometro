ce429d4bab1c71dc2dae6f49e9ade11b12e88ca6
Roberio (Camara) <roberio.rebeca@gmail.com>
2023-12-01 11:09:26 -0400

    alteracoes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -10,14 +11,32 @@ import (
 )
 
 const (
-	apiAuthToken = "seu_token_de_autenticacao_aqui"
+	apiAuthToken = "26c465ff73fcbbfc3e79f528e9cc59fde54081ba"
 )
+
+type Response struct {
+	CodReuniao                   int    `json:"codReuniao"`
+	CodReuniaoPrincipal          int    `json:"codReuniaoPrincipal"`
+	TxtTituloReuniao             string `json:"txtTituloReuniao"`
+	TxtSiglaOrgao                string `json:"txtSiglaOrgao"`
+	TxtApelido                   string `json:"txtApelido"`
+	TxtNomeOrgao                 string `json:"txtNomeOrgao"`
+	CodEstadoReuniao             int    `json:"codEstadoReuniao"`
+	TxtTipoReuniao               string `json:"txtTipoReuniao"`
+	TxtObjeto                    string `json:"txtObjeto"`
+	TxtLocal                     string `json:"txtLocal"`
+	BolReuniaoConjunta           bool   `json:"bolReuniaoConjunta"`
+	BolHabilitarEventoInterativo bool   `json:"bolHabilitarEventoInterativo"`
+	IDYoutube                    string `json:"idYoutube"`
+	CodEstadoTransmissaoYoutube  int    `json:"codEstadoTransmissaoYoutube"`
+	DatReuniaoString             string `json:"datReuniaoString"`
+}
 
 func main() {
 	router := gin.Default()
 
 	// Variável para armazenar o resultado anterior
-	var resultadoAnterior string
+	var resultadoAnterior bool
 
 	// Função para executar uma URL específica
 	executarURL := func(url string) {
@@ -35,7 +54,7 @@ func main() {
 	// Função para realizar a consulta à API com autenticação via token
 	consultaAPI := func() {
 		// Substitua a URL pela API que você deseja consultar
-		apiURL := "https://api.example.com/data"
+		apiURL := "https://sapl.sapezal.mt.leg.br/api/sessao-plenaria/21/"
 		req, err := http.NewRequest("GET", apiURL, nil)
 		if err != nil {
 			fmt.Println("Erro ao criar a solicitação à API:", err)
@@ -60,9 +79,13 @@ func main() {
 			return
 		}
 
+		var myStoredVariable Response
+		json.Unmarshal(body, &myStoredVariable)
+		// return myStoredVariable.BolHabilitarEventoInterativo
 		// Converte o corpo da resposta para string
-		resultadoAtual := string(body)
+		resultadoAtual := myStoredVariable.BolHabilitarEventoInterativo
 
+		fmt.Println("Resposta da API:", resultadoAtual)
 		// Verifica se houve uma mudança no resultado
 		if resultadoAtual != resultadoAnterior {
 			// Atualiza o resultado anterior
@@ -70,10 +93,10 @@ func main() {
 
 			// Lógica para executar diferentes URLs com base na resposta
 			switch resultadoAtual {
-			case "A":
-				executarURL("http://192.168.1.80/cron/start")
-			case "B":
-				executarURL("http://192.168.1.80/cron/stop")
+			case true:
+				executarURL("http://192.168.1.80/Bcron.htm?C1=2&f2=on&n4=0&n5=8&n6=0&Fc=CONTAR&f4=on")
+			case false:
+				executarURL("http://192.168.1.80/Bcron.htm?C1=2&f2=on&n4=0&n5=8&n6=0&Fp=PARAR&f4=on")
 			default:
 				fmt.Println("Resposta desconhecida da API:", resultadoAtual)
 			}
